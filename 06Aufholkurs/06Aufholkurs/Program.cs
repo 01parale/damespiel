ff6c43a25ceb34a18a4959b0e72f002adf88cc0d
01parale <01parale@rgtfo-me.it>
2018-03-14 22:42:25 +0100

    Schrittweite hinzugefügt

--- 06Aufholkurs/06Aufholkurs/Program.cs
+++ 06Aufholkurs/06Aufholkurs/Program.cs
@@ -7,8 +7,15 @@ namespace _06Aufholkurs
 {
     class Program
     {
+        public static int[,] feld = new int[9, 9];
+        public static int eingabeX, eingabeY, eingabeXnew, eingabeYnew;
+        public static bool spieler1_ist_dran = true;
+        public static string symbol2 = "O";
+        public static string symbol1 = "X";
+
         static void Main(string[] args)
         {
+            //Prepare game
             Console.SetCursorPosition(6, 2);
             Console.Write("12345678");              //8x8 feld
             for (int x = 0; x < 10; x++)
@@ -24,182 +31,167 @@ namespace _06Aufholkurs
                     Console.Write("|");
                     Console.SetCursorPosition(8 + 6, y + 3);
                     Console.Write("|");
-
-                    setzn1(x, y);
-                    setzn2(x, y);
                 }
                 Console.WriteLine();
             }
-            spieler = 0;
-            for (int i = 0; i < 100; i++)
+            createSpieler1();
+            createSpieler2();
+
+            //Game is ready
+            while (true)
             {
                 Console.SetCursorPosition(1, 16);
                 Console.WriteLine("Welchen Stein möchten Sie auswählen?");
-                eingabep = Convert.ToInt32(Console.ReadLine());
-                eingabep2 = Convert.ToInt32(Console.ReadLine());
+                eingabeX = Convert.ToInt32(Console.ReadLine());
+                eingabeY = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Geben Sie die neue Position ein");
-                eingabepn = Convert.ToInt32(Console.ReadLine());
-                eingabepn2 = Convert.ToInt32(Console.ReadLine());
+                eingabeXnew = Convert.ToInt32(Console.ReadLine());
+                eingabeYnew = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine();
 
-                if(feld[eingabepn,eingabepn2] == 0)                         //int array bei deklaration leere felder auf 0
+                if (eingabeX > 0 && eingabeXnew > 0 && eingabeX < 9 && eingabeXnew < 9 && eingabeY > 0 && eingabeYnew > 0 && eingabeY < 9 && eingabeYnew < 9 && feld[eingabeXnew, eingabeYnew] == 0 && feld[eingabeX, eingabeY] == (spieler1_ist_dran ? 1 : 2))                         //int array bei deklaration leere felder auf 0
                 {
-                    if(eingabep == eingabepn)
+                    if (eingabeX == eingabeXnew && (eingabeY + 1 == eingabeYnew || eingabeY - 1 == eingabeYnew))
                     {
-                        kontrolle(eingabepn, eingabepn2);
-                        kontrolle2(eingabep, eingabep2);
-                        feld[eingabep, eingabep2] = 1;
+                        print(eingabeXnew, eingabeYnew, (spieler1_ist_dran ? symbol1 : symbol2));
+                        clear(eingabeX, eingabeY);
+                        //Clear Fehler-Anzeige
                         Console.SetCursorPosition(1, 1);
                         Console.WriteLine("      ");
                     }
-                    else if((eingabep + 2 == eingabepn && eingabep2 + 2 == eingabepn2||eingabep + 2 == eingabepn && eingabep2 - 2 == eingabepn2||eingabep - 2 == eingabepn && eingabep2 + 2 == eingabepn2||eingabep - 2 == eingabepn && eingabep2 - 2 == eingabepn2) && (feld[eingabep + 1, eingabep2 + 1] != 0 || feld[eingabep + 1, eingabep2 - 1] != 0 ||feld[eingabep - 1, eingabep2 + 1] != 0 ||feld[eingabep - 1, eingabep2 - 1] != 0 ))
+                    else if ((eingabeX + 2 == eingabeXnew && eingabeY + 2 == eingabeYnew || eingabeX + 2 == eingabeXnew && eingabeY - 2 == eingabeYnew || eingabeX - 2 == eingabeXnew && eingabeY + 2 == eingabeYnew || eingabeX - 2 == eingabeXnew && eingabeY - 2 == eingabeYnew) && (feld[eingabeX + 1, eingabeY + 1] != 0 || feld[eingabeX + 1, eingabeY - 1] != 0 || feld[eingabeX - 1, eingabeY + 1] != 0 || feld[eingabeX - 1, eingabeY - 1] != 0))
                     {                                                                                                                                                                                                                                                    //im int array alles auf 0 so kann man mit 0 nicht abfragen!                                                           
-                        kontrolle(eingabepn, eingabepn2);
-                        kontrolle2(eingabep, eingabep2);
+                        print(eingabeXnew, eingabeYnew, (spieler1_ist_dran ? symbol1 : symbol2));
+                        clear(eingabeX, eingabeY);
+                        if (eingabeX > eingabeXnew && eingabeY > eingabeYnew)
+                            clear(eingabeX - 1, eingabeY - 1);
+                        else if (eingabeX > eingabeXnew && eingabeY < eingabeYnew)
+                            clear(eingabeX - 1, eingabeY + 1);
+                        else if (eingabeX < eingabeXnew && eingabeY > eingabeYnew)
+                            clear(eingabeX + 1, eingabeY - 1);
+                        else if (eingabeX < eingabeXnew && eingabeY < eingabeYnew)
+                            clear(eingabeX + 1, eingabeY + 1);
+
+                        //Clear Fehler-Anzeige
                         Console.SetCursorPosition(1, 1);
                         Console.WriteLine("      ");
-                        fressen(eingabep, eingabep2);
                     }
                     else
                     {
+                        //Fehler-Anzeige
                         Console.SetCursorPosition(1, 1);
                         Console.WriteLine("Fehler");
+                        continue;
                     }
                 }
                 else
                 {
-                    Console.SetCursorPosition(1,1);
+                    //Fehler-Anzeige
+                    Console.SetCursorPosition(1, 1);
                     Console.WriteLine("Fehler");
+                    continue;
                 }
-                feld[eingabep, eingabep2] = 0;
-                spieler++;
+                //invertiere spieler der dran ist
+                spieler1_ist_dran = !spieler1_ist_dran;
             }
-            Console.ReadKey();
         }
-        public static int[,] feld = new int[9, 9];
-        public static int eingabep, eingabep2, eingabepn, eingabepn2, spieler;
-        static void setzn1(int x, int y)
+
+        static void createSpieler1()
         {
-            string spieler_1 = "X";
             Console.SetCursorPosition(5 + 1, 3 + 6);
             feld[1, 3] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 3, 3 + 6);
             feld[3, 6] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 5, 3 + 6);
             feld[5, 6] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 7, 3 + 6);
             feld[7, 6] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 1, 3 + 6);
             feld[1, 6] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 2, 3 + 7);
             feld[2, 7] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 4, 3 + 7);
             feld[4, 7] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 6, 3 + 7);
             feld[6, 7] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 8, 3 + 7);
             feld[8, 7] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 1, 3 + 8);
             feld[1, 8] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 3, 3 + 8);
             feld[3, 8] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 5, 3 + 8);
             feld[5, 8] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
             Console.SetCursorPosition(5 + 7, 3 + 8);
             feld[7, 8] = 1;
-            Console.Write(spieler_1);
+            Console.Write(symbol1);
         }
-        static void setzn2(int x, int y)
+
+        static void createSpieler2()
         {
-            string spieler_2 = "O";
             Console.SetCursorPosition(5 + 2, 3 + 1);
-            feld[2, 1] = 1;
-            Console.Write(spieler_2);
+            feld[2, 1] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 4, 3 + 1);
-            feld[4, 1] = 1;
-            Console.Write(spieler_2);
+            feld[4, 1] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 6, 3 + 1);
-            feld[6, 1] = 1;
-            Console.Write(spieler_2);
+            feld[6, 1] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 8, 3 + 1);
-            feld[8, 1] = 1;
-            Console.Write(spieler_2);
+            feld[8, 1] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 1, 3 + 2);
-            feld[1, 2] = 1;
-            Console.Write(spieler_2);
+            feld[1, 2] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 3, 3 + 2);
-            feld[3, 2] = 1;
-            Console.Write(spieler_2);
+            feld[3, 2] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 5, 3 + 2);
-            feld[5, 2] = 1;
-            Console.Write(spieler_2);
+            feld[5, 2] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 7, 3 + 2);
-            feld[7, 2] = 1;
-            Console.Write(spieler_2);
+            feld[7, 2] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 2, 3 + 3);
-            feld[2, 3] = 1;
-            Console.Write(spieler_2);
+            feld[2, 3] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 4, 3 + 3);
-            feld[4, 3] = 1;
-            Console.Write(spieler_2);
+            feld[4, 3] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 6, 3 + 3);
-            feld[6, 3] = 1;
-            Console.Write(spieler_2);
+            feld[6, 3] = 2;
+            Console.Write(symbol2);
             Console.SetCursorPosition(5 + 8, 3 + 3);
-            feld[8, 3] = 1;
-            Console.Write(spieler_2);
-        }
-        static void kontrolle(int x, int y)
-        {
-            
-                if (spieler % 2 == 0)
-                {
-                    string spieler_2 = "O";
-                    Console.SetCursorPosition(eingabepn + 5, eingabepn2 + 3);
-                    feld[eingabepn, eingabepn2] = 1;
-                    Console.Write(spieler_2);
-                }
-                else
-                {
-                    string spieler_1 = "X";
-                    Console.SetCursorPosition(eingabepn + 5, eingabepn2 + 3);
-                    feld[eingabepn, eingabepn2] = 1;
-                    Console.Write(spieler_1);
-                }
-            
-        }
-        static void kontrolle2(int x, int y)
-        {
-                string spieler3 = " ";
-                Console.SetCursorPosition(eingabep + 5, eingabep2 + 3);
-                feld[eingabepn, eingabepn2] = 0;
-                Console.Write(spieler3);
-        }
-        static void fressen(int x, int y)//Beim fressen wird das z.B feld[2,6] auf 0 gesetzt jedoch bleibt das X  dort stehen...jedoch wenn man von diesem punkt aus den stein X verschieben möchte bleibt der stein erhalten und es wird nur ein nuer stein erzeugt! Hingegen eine überschreibung ist möglich!
-        {
-            //if(feld[eingabep, eingabep2] == 1 && feld[eingabep + 1, eingabep2 + 1] == 1)
-
-            if (feld[eingabep, eingabep2] == 1)
-                feld[eingabepn, eingabepn2] = 2;
-
-            feld[eingabep + 1, eingabep2 + 1] = 0;
-            string spieler3 = " ";
-            Console.SetCursorPosition(eingabep + 1, eingabep2 + 1);
-            Console.Write(spieler3);
+            feld[8, 3] = 2;
+            Console.Write(symbol2);
         }
 
+        static void print(int x, int y, string symbol)
+        {
+            Console.SetCursorPosition(x + 5, y + 3);
+            Console.Write(symbol);
+            feld[x, y] = (symbol == symbol1 ? 1 : 2);
+        }
+
+        static void clear(int x, int y)
+        {
+            Console.SetCursorPosition(x + 5, y + 3);
+            feld[x, y] = 0;
+            Console.Write(" ");
+        }
     }
 }
 
