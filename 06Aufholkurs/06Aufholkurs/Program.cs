436cce76b8f404f226baed5444f76c513f4a7e07
01parale <01parale@rgtfo-me.it>
2018-04-12 19:10:46 +0200

    Ausbesserung des Feldes und erstellen der Spielinfo

--- 06Aufholkurs/06Aufholkurs/Program.cs
+++ 06Aufholkurs/06Aufholkurs/Program.cs
@@ -8,7 +8,9 @@ namespace _06Aufholkurs
     class Program
     {
         public static int[,] feld = new int[9, 9];
-        public static int eingabeX, eingabeY, eingabeXnew, eingabeYnew;
+        public static string EingabeX, EingabeXnew;
+        public static int eingabeX, eingabeXnew;
+        public static int eingabeY, eingabeYnew;
         public static bool spieler1_ist_dran = true;
         public static string symbol2 = "O";
         public static string symbol1 = "X";
@@ -18,7 +20,7 @@ namespace _06Aufholkurs
             //Prepare game
             //erstellen der Beschriftung
             Console.SetCursorPosition(2, 2);
-            Console.Write("X   12345678");              //8x8 feld
+            Console.Write("X   abcdefgh");              //8x8 feld
             Console.SetCursorPosition(0, 3);
             int a = 0;
             Console.WriteLine("Y");
@@ -35,16 +37,20 @@ namespace _06Aufholkurs
             Console.SetCursorPosition(40, 6);
             Console.Write("Jetzt das gleiche nochmal mit dem neuen Punkt...");
             Console.SetCursorPosition(40, 7);
-            Console.Write("Es kann immer nur auf der Y-Achse verschoben werden...");
+            Console.Write("Es wird immer X&Y verschoben...");
             Console.SetCursorPosition(40, 8);
             Console.Write("Auch dabei immer nur einen Schritt...außer");
             Console.SetCursorPosition(40, 9);
-            Console.Write("Beim fressen darf weiter gesprungen werdn und auch in alle richtungen...");
+            Console.Write("Beim Fressen darf weiter gesprungen werdn und auch in alle Richtungen...");
+            Console.SetCursorPosition(40, 10);
+            Console.Write("Bei einem Fehler ist dennoch der selbe Spieler dran...");
+            Console.SetCursorPosition(40, 11);
+            Console.Write("Auf den Fehler wird hingewiesen...oberhalb des Spielfeldes...");
 
             //Create Spielfeld
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < 9; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int y = 0; y < 9; y++)
                 {
                     Console.SetCursorPosition(x + 5, 0 + 3);
                     Console.Write("¯");
@@ -58,6 +64,7 @@ namespace _06Aufholkurs
                 }
                 Console.WriteLine();
             }
+            //Create Spieler
             createSpieler1();
             createSpieler2();
 
@@ -68,25 +75,28 @@ namespace _06Aufholkurs
             {
                 //Spielstein auswahl & Verschiebungspunkt
                 Console.SetCursorPosition(1, 16);
-                Console.WriteLine("Welchen Stein möchten Sie auswählen?");
-                eingabeX = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine("Welchen Stein möchten Sie auswählen?");
+                Console.Write("Welchen Stein möchten sie auswählen?\n");
+                EingabeX = Console.ReadLine();
                 eingabeY = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Geben Sie die neue Position ein");
-                eingabeXnew = Convert.ToInt32(Console.ReadLine());
+                EingabeXnew = Console.ReadLine();
                 eingabeYnew = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine();
-                
+                convertzuzahlen();
+
                 //Abfrage bewewegung in leeres Feld und ob angegebener Punkt sich im Spielfeld befindet
                 if (eingabeX > 0 && eingabeXnew > 0 && eingabeX < 9 && eingabeXnew < 9 && eingabeY > 0 && eingabeYnew > 0 && eingabeY < 9 && eingabeYnew < 9 && feld[eingabeXnew, eingabeYnew] == 0 && feld[eingabeX, eingabeY] == (spieler1_ist_dran ? 1 : 2))                         //int array bei deklaration leere felder auf 0
                 {
                     //Abfrage ob X-Achsenwert gleichgebliben ist und ob Y-Achsenwert sich nur um 1 verschoben hat 
-                    if (eingabeX == eingabeXnew && (eingabeY + 1 == eingabeYnew || eingabeY - 1 == eingabeYnew))
+                    if ((eingabeX + 1 == eingabeXnew || eingabeX - 1 == eingabeXnew) && (eingabeY + 1 == eingabeYnew || eingabeY - 1 == eingabeYnew))
                     {
                         print(eingabeXnew, eingabeYnew, (spieler1_ist_dran ? symbol1 : symbol2));
                         clear(eingabeX, eingabeY);
                         //Clear Fehler-Anzeige
                         Console.SetCursorPosition(1, 1);
-                        Console.WriteLine("                  ");
+                        Console.WriteLine("                                                                           ");
+
                     }
                     //Abfrage zum fressen...dh ob ein zu fresender Spielstein vorhanden ist und ob man den richtig schritt gemacht hat...
                     else if ((eingabeX + 2 == eingabeXnew && eingabeY + 2 == eingabeYnew || eingabeX + 2 == eingabeXnew && eingabeY - 2 == eingabeYnew || eingabeX - 2 == eingabeXnew && eingabeY + 2 == eingabeYnew || eingabeX - 2 == eingabeXnew && eingabeY - 2 == eingabeYnew) && (feld[eingabeX + 1, eingabeY + 1] != 0 || feld[eingabeX + 1, eingabeY - 1] != 0 || feld[eingabeX - 1, eingabeY + 1] != 0 || feld[eingabeX - 1, eingabeY - 1] != 0))
@@ -96,21 +106,23 @@ namespace _06Aufholkurs
                         fressen(eingabeX, eingabeY, eingabeXnew, eingabeYnew);
                         //Clear Fehler-Anzeige
                         Console.SetCursorPosition(1, 1);
-                        Console.WriteLine("       ");
-                    }                      
+                        Console.WriteLine("                                                                           ");
+                    }
                     else
                     {
                         //Fehler-Anzeige
+                        Console.WriteLine("                                                                           ");
                         Console.SetCursorPosition(1, 1);
-                        Console.WriteLine("Fehler!           ");
+                        Console.WriteLine("Fehler! Es kann nur schräg Gesprungen werden und immer nur ein feld weiter");
                         continue;
                     }
                 }
                 else
                 {
                     //Fehler-Anzeige
+                    Console.WriteLine("                                                                           ");
                     Console.SetCursorPosition(1, 1);
-                    Console.WriteLine("Fehler!           ");
+                    Console.WriteLine("Fehler! Ausßerhalb des Spielfeldes bzw Stein im Weg                        ");
                     continue;
                 }
                 //invertiere spieler der dran ist
@@ -202,7 +214,7 @@ namespace _06Aufholkurs
         }
 
         static void print(int x, int y, string symbol)    //Ausgabe am neuen Punkt
-        {                                              
+        {
             Console.SetCursorPosition(x + 5, y + 3);
             Console.Write(symbol);
             feld[x, y] = (symbol == symbol1 ? 1 : 2);
@@ -224,8 +236,64 @@ namespace _06Aufholkurs
             else if (x < xnew && y > ynew)
                 clear(x + 1, y - 1);
             else if (x < xnew && y < ynew)
-                clear(x + 1, y + 1);  
+                clear(x + 1, y + 1);
         }
+
+        static void convertzuzahlen()
+        {
+            //EingabeX(string) to eingabeX(int) Converten
+            if (EingabeX == "a")
+                eingabeX = 1;
+
+            if (EingabeX == "b")
+                eingabeX = 2;
+
+            if (EingabeX == "c")
+                eingabeX = 3;
+
+            if (EingabeX == "d")
+                eingabeX = 4;
+
+            if (EingabeX == "e")
+                eingabeX = 5;
+
+            if (EingabeX == "f")
+                eingabeX = 6;
+
+            if (EingabeX == "g")
+                eingabeX = 7;
+
+            if (EingabeX == "h")
+                eingabeX = 8;
+
+            //EingabeXnew(string) to eingabeXnew(int) Converten
+            if (EingabeXnew == "a")
+                eingabeXnew = 1;
+
+            if (EingabeXnew == "b")
+                eingabeXnew = 2;
+
+            if (EingabeXnew == "c")
+                eingabeXnew = 3;
+
+            if (EingabeXnew == "d")
+                eingabeXnew = 4;
+
+            if (EingabeXnew == "e")
+                eingabeXnew = 5;
+
+            if (EingabeXnew == "f")
+                eingabeXnew = 6;
+
+            if (EingabeXnew == "g")
+                eingabeXnew = 7;
+
+            if (EingabeXnew == "h")
+                eingabeXnew = 8;
+
+        }
+
     }
 }
+
 
